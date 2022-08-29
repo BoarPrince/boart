d96c60a700c8fc76518412bbc57cc430b8f617ab
Markus Kern <mk@jitpay.eu>
2022-08-29 10:17:21 +0200

    improve reference structure

--- jest.config.ts
+++ jest.config.ts
@@ -18,11 +18,18 @@ const config: Config.InitialOptions = {
         }
     },
     testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts)$',
-    modulePathIgnorePatterns: ['dist'],
+    modulePathIgnorePatterns: ['./src/packagages/.*/dist/', './dist/'],
+    coveragePathIgnorePatterns: ['node_modules'],
     moduleFileExtensions: ['ts', 'js', 'json'],
     collectCoverageFrom: ['**/src/*/**/*.ts', '!**/src/*/**/*.enum.ts', '!**/src/*/**/*.d.ts'],
-    coveragePathIgnorePatterns: ['node_modules'],
-    coverageReporters: ['json', 'lcov', 'text', 'clover']
+    coverageReporters: ['json', 'lcov', 'text', 'clover'],
+    moduleNameMapper: {
+        '^@boart/core$': '<rootDir>/src/packages/core/src/index.ts',
+        '^@boart/core-impl$': '<rootDir>/src/packages/core-impl/src/index.ts',
+        '^@boart/execution$': '<rootDir>/src/packages/execution/src/index.ts',
+        '^@boart/protocol$': '<rootDir>/src/packages/protocol/src/index.ts',
+        '^@boart/basic$': '<rootDir>/src/packages/basic/src/index.ts'
+    }
     // setupFiles: [
     //     './setupJest.js'
     //   ]
