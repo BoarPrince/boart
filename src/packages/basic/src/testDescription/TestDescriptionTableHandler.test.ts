1836315522abd4ea4462aa2f667d255813fc94e5
Markus Kern <mk@jitpay.eu>
2022-10-03 09:12:53 +0200

    re-factor testdescription tests

--- src/packages/basic/src/testDescription/TestDescriptionTableHandler.test.ts
+++ src/packages/basic/src/testDescription/TestDescriptionTableHandler.test.ts
@@ -63,269 +63,281 @@ jest.mock('fs');
 /**
  *
  */
-it('default description', async () => {
-    const tableRows = MarkdownTableReader.convert(
-        `|action       |value   |
-         |-------------|--------|
-         | description | desc   |
-         | priority    | medium |`
-    );
+describe('description', () => {
+    /**
+     *
+     */
+    it('default description', async () => {
+        const tableRows = MarkdownTableReader.convert(
+            `|action       |value   |
+             |-------------|--------|
+             | description | desc   |
+             | priority    | medium |`
+        );
 
-    await sut.handler.process(tableRows);
+        await sut.handler.process(tableRows);
 
-    expect(sut.handler.executionEngine.context.config.description).toEqual('desc');
-    expect(sut.handler.executionEngine.context.config.priority).toEqual('medium');
+        expect(sut.handler.executionEngine.context.config.description).toEqual('desc');
+        expect(sut.handler.executionEngine.context.config.priority).toEqual('medium');
+    });
+
+    /**
+     *
+     */
+    it('with ticket', async () => {
+        const tableRows = MarkdownTableReader.convert(
+            `|action       |value   |
+             |-------------|--------|
+             | description | desc   |
+             | priority    | medium |
+             | ticket      | t-01   |`
+        );
+
+        await sut.handler.process(tableRows);
+
+        expect(sut.handler.executionEngine.context.config.description).toEqual('desc');
+        expect(sut.handler.executionEngine.context.config.priority).toEqual('medium');
+        expect(sut.handler.executionEngine.context.config.ticket).toEqual('t-01');
+    });
+
+    /**
+     *
+     */
+    it('with two tickets - one row', async () => {
+        const tableRows = MarkdownTableReader.convert(
+            `|action       |value       |
+             |-------------|------------|
+             | description | desc       |
+             | priority    | medium     |
+             | ticket      | t-01, t-02 |`
+        );
+
+        await sut.handler.process(tableRows);
+
+        expect(sut.handler.executionEngine.context.config.description).toEqual('desc');
+        expect(sut.handler.executionEngine.context.config.priority).toEqual('medium');
+        expect(sut.handler.executionEngine.context.config.ticket).toEqual('t-01, t-02');
+    });
+
+    /**
+     *
+     */
+    it('with two tickets - two rows', async () => {
+        const tableRows = MarkdownTableReader.convert(
+            `|action       |value   |
+             |-------------|--------|
+             | description | desc   |
+             | priority    | medium |
+             | ticket      | t-01   |
+             | ticket      | t-02   |`
+        );
+
+        await sut.handler.process(tableRows);
+
+        expect(sut.handler.executionEngine.context.config.description).toEqual('desc');
+        expect(sut.handler.executionEngine.context.config.priority).toEqual('medium');
+        expect(sut.handler.executionEngine.context.config.ticket).toEqual('t-01,t-02');
+    });
+
+    /**
+     *
+     */
+    it('with two tickets - two rows, but one definition, ', async () => {
+        const tableRows = MarkdownTableReader.convert(
+            `|action       |value   |
+             |-------------|--------|
+             | description | desc   |
+             | priority    | medium |
+             | ticket      | t-01   |
+             |             | t-02   |`
+        );
+
+        await sut.handler.process(tableRows);
+
+        expect(sut.handler.executionEngine.context.config.description).toEqual('desc');
+        expect(sut.handler.executionEngine.context.config.priority).toEqual('medium');
+        expect(sut.handler.executionEngine.context.config.ticket).toEqual('t-01,t-02');
+    });
+
+    /**
+     *
+     */
+    it('with ticket - other provider - check report', async () => {
+        const tableRows = MarkdownTableReader.convert(
+            `|action       |value            |
+             |-------------|-----------------|
+             | description | desc            |
+             | priority    | medium          |
+             | ticket      | provider-x:t-01 |`
+        );
+
+        await sut.handler.process(tableRows);
+
+        expect(sut.handler.executionEngine.context.config.description).toEqual('desc');
+        expect(sut.handler.executionEngine.context.config.priority).toEqual('medium');
+        expect(sut.handler.executionEngine.context.config.ticket).toEqual('provider-x:t-01');
+
+        Runtime.instance.testRuntime.current.id = 'id-id-id';
+        TestReport.instance.report();
+        expect(fs.writeFile).toBeCalledWith(
+            'id-id-id.json',
+            JSON.stringify({
+                id: 'id-id-id',
+                number: '',
+                name: '',
+                status: 2,
+                priority: 1,
+                startTime: '2020-01-01T00:00:00.000Z',
+                tickets: [{ id: 't-01', link: 'ticket_link_provider-x', source: 'Provider-x' }],
+                descriptions: 'desc',
+                failureDescription: ''
+            }),
+            'utf-8',
+            expect.any(Function)
+        );
+    });
+
+    /**
+     *
+     */
+    it('with ticket - check report', async () => {
+        const tableRows = MarkdownTableReader.convert(
+            `|action       |value   |
+             |-------------|--------|
+             | description | desc   |
+             | priority    | medium |
+             | ticket      | t-01   |`
+        );
+
+        await sut.handler.process(tableRows);
+
+        expect(sut.handler.executionEngine.context.config.description).toEqual('desc');
+        expect(sut.handler.executionEngine.context.config.priority).toEqual('medium');
+        expect(sut.handler.executionEngine.context.config.ticket).toEqual('t-01');
+
+        Runtime.instance.testRuntime.current.id = 'id-id-id';
+        TestReport.instance.report();
+        expect(fs.writeFile).toBeCalledWith(
+            'id-id-id.json',
+            JSON.stringify({
+                id: 'id-id-id',
+                number: '',
+                name: '',
+                status: 2,
+                priority: 1,
+                startTime: '2020-01-01T00:00:00.000Z',
+                tickets: [{ id: 't-01', link: 'ticket_link_default', source: 'ticket_source_default' }],
+                descriptions: 'desc',
+                failureDescription: ''
+            }),
+            'utf-8',
+            expect.any(Function)
+        );
+    });
+
+    /**
+     *
+     */
+    it('with multiple descriptions', async () => {
+        const tableRows = MarkdownTableReader.convert(
+            `|action       |value   |
+             |-------------|--------|
+             | description | desc1  |
+             | description | desc2  |
+             | description | desc3  |
+             |             | desc4  |
+             | priority    | medium |`
+        );
+
+        await sut.handler.process(tableRows);
+
+        expect(sut.handler.executionEngine.context.config.description).toEqual('desc1\ndesc2\ndesc3\ndesc4');
+        expect(sut.handler.executionEngine.context.config.priority).toEqual('medium');
+        expect(sut.handler.executionEngine.context.config.ticket).toEqual('');
+
+        Runtime.instance.testRuntime.current.id = 'id-id-id';
+        TestReport.instance.report();
+        expect(fs.writeFile).toBeCalledWith(
+            'id-id-id.json',
+            JSON.stringify({
+                id: 'id-id-id',
+                number: '',
+                name: '',
+                status: 2,
+                priority: 1,
+                startTime: '2020-01-01T00:00:00.000Z',
+                tickets: [],
+                descriptions: 'desc1\ndesc2\ndesc3\ndesc4',
+                failureDescription: ''
+            }),
+            'utf-8',
+            expect.any(Function)
+        );
+    });
 });
 
 /**
  *
  */
-it('with ticket', async () => {
-    const tableRows = MarkdownTableReader.convert(
-        `|action       |value   |
-         |-------------|--------|
-         | description | desc   |
-         | priority    | medium |
-         | ticket      | t-01   |`
-    );
+describe('error handling', () => {
+    /**
+     *
+     */
+    it('missing priority', async () => {
+        const tableRows = MarkdownTableReader.convert(
+            `|action       |value  |
+             |-------------|-------|
+             | description | desc  |`
+        );
 
-    await sut.handler.process(tableRows);
+        await expect(async () => await sut.handler.process(tableRows)).rejects.toThrowError("Key 'priority' is required, but it's missing");
+    });
 
-    expect(sut.handler.executionEngine.context.config.description).toEqual('desc');
-    expect(sut.handler.executionEngine.context.config.priority).toEqual('medium');
-    expect(sut.handler.executionEngine.context.config.ticket).toEqual('t-01');
-});
+    /**
+     *
+     */
+    it('missing description', async () => {
+        const tableRows = MarkdownTableReader.convert(
+            `|action    |value   |
+             |----------|--------|
+             | priority | medium |`
+        );
 
-/**
- *
- */
-it('with two tickets - one row', async () => {
-    const tableRows = MarkdownTableReader.convert(
-        `|action       |value       |
-         |-------------|------------|
-         | description | desc       |
-         | priority    | medium     |
-         | ticket      | t-01, t-02 |`
-    );
+        await expect(async () => await sut.handler.process(tableRows)).rejects.toThrowError(
+            "Key 'description' is required, but it's missing"
+        );
+    });
 
-    await sut.handler.process(tableRows);
+    /**
+     *
+     */
+    it('priority must be unique', async () => {
+        const tableRows = MarkdownTableReader.convert(
+            `|action       |value   |
+             |-------------|--------|
+             | description | desc   |
+             | priority    | xx1    |
+             | priority    | xx2    |`
+        );
 
-    expect(sut.handler.executionEngine.context.config.description).toEqual('desc');
-    expect(sut.handler.executionEngine.context.config.priority).toEqual('medium');
-    expect(sut.handler.executionEngine.context.config.ticket).toEqual('t-01, t-02');
-});
+        await expect(async () => await sut.handler.process(tableRows)).rejects.toThrowError(
+            "Validator: 'UniqueValidator' => key 'priority' occurs 2 times"
+        );
+    });
 
-/**
- *
- */
-it('with two tickets - two rows', async () => {
-    const tableRows = MarkdownTableReader.convert(
-        `|action       |value   |
-         |-------------|--------|
-         | description | desc   |
-         | priority    | medium |
-         | ticket      | t-01   |
-         | ticket      | t-02   |`
-    );
+    /**
+     *
+     */
+    it('wrong priority value', async () => {
+        const tableRows = MarkdownTableReader.convert(
+            `|action       |value   |
+             |-------------|--------|
+             | description | desc   |
+             | priority    | xxx    |`
+        );
 
-    await sut.handler.process(tableRows);
-
-    expect(sut.handler.executionEngine.context.config.description).toEqual('desc');
-    expect(sut.handler.executionEngine.context.config.priority).toEqual('medium');
-    expect(sut.handler.executionEngine.context.config.ticket).toEqual('t-01,t-02');
-});
-
-/**
- *
- */
-it('with two tickets - two rows, but one definition, ', async () => {
-    const tableRows = MarkdownTableReader.convert(
-        `|action       |value   |
-         |-------------|--------|
-         | description | desc   |
-         | priority    | medium |
-         | ticket      | t-01   |
-         |             | t-02   |`
-    );
-
-    await sut.handler.process(tableRows);
-
-    expect(sut.handler.executionEngine.context.config.description).toEqual('desc');
-    expect(sut.handler.executionEngine.context.config.priority).toEqual('medium');
-    expect(sut.handler.executionEngine.context.config.ticket).toEqual('t-01,t-02');
-});
-
-/**
- *
- */
-it('with ticket - other provider - check report', async () => {
-    const tableRows = MarkdownTableReader.convert(
-        `|action       |value            |
-         |-------------|-----------------|
-         | description | desc            |
-         | priority    | medium          |
-         | ticket      | provider-x:t-01 |`
-    );
-
-    await sut.handler.process(tableRows);
-
-    expect(sut.handler.executionEngine.context.config.description).toEqual('desc');
-    expect(sut.handler.executionEngine.context.config.priority).toEqual('medium');
-    expect(sut.handler.executionEngine.context.config.ticket).toEqual('provider-x:t-01');
-
-    Runtime.instance.testRuntime.current.id = 'id-id-id';
-    TestReport.instance.report();
-    expect(fs.writeFile).toBeCalledWith(
-        'id-id-id.json',
-        JSON.stringify({
-            id: 'id-id-id',
-            number: '',
-            name: '',
-            status: 2,
-            priority: 1,
-            startTime: '2020-01-01T00:00:00.000Z',
-            tickets: [{ id: 't-01', link: 'ticket_link_provider-x', source: 'Provider-x' }],
-            descriptions: 'desc',
-            failureDescription: ''
-        }),
-        'utf-8',
-        expect.any(Function)
-    );
-});
-
-/**
- *
- */
-it('with ticket - check report', async () => {
-    const tableRows = MarkdownTableReader.convert(
-        `|action       |value   |
-         |-------------|--------|
-         | description | desc   |
-         | priority    | medium |
-         | ticket      | t-01   |`
-    );
-
-    await sut.handler.process(tableRows);
-
-    expect(sut.handler.executionEngine.context.config.description).toEqual('desc');
-    expect(sut.handler.executionEngine.context.config.priority).toEqual('medium');
-    expect(sut.handler.executionEngine.context.config.ticket).toEqual('t-01');
-
-    Runtime.instance.testRuntime.current.id = 'id-id-id';
-    TestReport.instance.report();
-    expect(fs.writeFile).toBeCalledWith(
-        'id-id-id.json',
-        JSON.stringify({
-            id: 'id-id-id',
-            number: '',
-            name: '',
-            status: 2,
-            priority: 1,
-            startTime: '2020-01-01T00:00:00.000Z',
-            tickets: [{ id: 't-01', link: 'ticket_link_default', source: 'ticket_source_default' }],
-            descriptions: 'desc',
-            failureDescription: ''
-        }),
-        'utf-8',
-        expect.any(Function)
-    );
-});
-
-/**
- *
- */
-it('with multiple descriptions', async () => {
-    const tableRows = MarkdownTableReader.convert(
-        `|action       |value   |
-         |-------------|--------|
-         | description | desc1  |
-         | description | desc2  |
-         | description | desc3  |
-         |             | desc4  |
-         | priority    | medium |`
-    );
-
-    await sut.handler.process(tableRows);
-
-    expect(sut.handler.executionEngine.context.config.description).toEqual('desc1\ndesc2\ndesc3\ndesc4');
-    expect(sut.handler.executionEngine.context.config.priority).toEqual('medium');
-    expect(sut.handler.executionEngine.context.config.ticket).toEqual('');
-
-    Runtime.instance.testRuntime.current.id = 'id-id-id';
-    TestReport.instance.report();
-    expect(fs.writeFile).toBeCalledWith(
-        'id-id-id.json',
-        JSON.stringify({
-            id: 'id-id-id',
-            number: '',
-            name: '',
-            status: 2,
-            priority: 1,
-            startTime: '2020-01-01T00:00:00.000Z',
-            tickets: [],
-            descriptions: 'desc1\ndesc2\ndesc3\ndesc4',
-            failureDescription: ''
-        }),
-        'utf-8',
-        expect.any(Function)
-    );
-});
-
-/**
- *
- */
-it('missing priority', async () => {
-    const tableRows = MarkdownTableReader.convert(
-        `|action       |value  |
-         |-------------|-------|
-         | description | desc  |`
-    );
-
-    await expect(async () => await sut.handler.process(tableRows)).rejects.toThrowError("Key 'priority' is required, but it's missing");
-});
-
-/**
- *
- */
-it('missing description', async () => {
-    const tableRows = MarkdownTableReader.convert(
-        `|action    |value   |
-         |----------|--------|
-         | priority | medium |`
-    );
-
-    await expect(async () => await sut.handler.process(tableRows)).rejects.toThrowError("Key 'description' is required, but it's missing");
-});
-
-/**
- *
- */
-it('priority must be unique', async () => {
-    const tableRows = MarkdownTableReader.convert(
-        `|action       |value   |
-         |-------------|--------|
-         | description | desc   |
-         | priority    | xx1    |
-         | priority    | xx2    |`
-    );
-
-    await expect(async () => await sut.handler.process(tableRows)).rejects.toThrowError(
-        "Validator: 'UniqueValidator' => key 'priority' occurs 2 times"
-    );
-});
-
-/**
- *
- */
-it('wrong priority value', async () => {
-    const tableRows = MarkdownTableReader.convert(
-        `|action       |value   |
-         |-------------|--------|
-         | description | desc   |
-         | priority    | xxx    |`
-    );
-
-    await expect(async () => await sut.handler.process(tableRows)).rejects.toThrowError(
-        "Value 'xxx' of key/column: 'priority/value' is not defined. Allowed is 'low' or 'medium' or 'high'"
-    );
+        await expect(async () => await sut.handler.process(tableRows)).rejects.toThrowError(
+            "Value 'xxx' of key/column: 'priority/value' is not defined. Allowed is 'low' or 'medium' or 'high'"
+        );
+    });
 });
