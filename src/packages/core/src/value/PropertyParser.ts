4736adefc6277463dac7d605ef5c28883c0b0627
Markus Kern <mk@jitpay.eu>
2022-11-22 23:56:21 +0100

    redo value replacer selector changes

--- src/packages/core/src/value/PropertyParser.ts
+++ src/packages/core/src/value/PropertyParser.ts
@@ -15,14 +15,15 @@ export class PropertyParser {
     /**
      *
      */
-    private constructor(
-        readonly replacer: string,
-        readonly isOptional: boolean,
-        readonly scope: string,
-        readonly property: string,
-        readonly selector: string,
-        readonly defaultOp: DefaultOp
-    ) {}
+    private constructor(readonly replacer: string, readonly isOptional: boolean, readonly scope: string, readonly name: string) {}
+    // private constructor(
+    //     readonly replacer: string,
+    //     readonly isOptional: boolean,
+    //     readonly scope: string,
+    //     readonly property: string,
+    //     readonly selector: string,
+    //     readonly defaultOp: DefaultOp
+    // ) {}
 
     /**
      *
@@ -40,21 +41,22 @@ export class PropertyParser {
      *
      */
     public static parse(value: string): PropertyParser {
-        const re = new RegExp(
-            /\${(?<replacer>[^{}?:]+?)(?<optional>[?]?)(:(?<scope>[glts]))?(:(?<property>[^{}#:]+))?(#(?<selector>[^{}#:]+))?((?<defaultOperator>:[^\w\d])(?<default>[^{}]+))?}/
-        );
-
+        // const re = new RegExp(
+        //     /\${(?<replacer>[^{}?:]+?)(?<optional>[?]?)(:(?<scope>[glts]))?(:(?<property>[^{}#:]+))?(#(?<selector>[^{}#:]+))?((?<defaultOperator>:[^\w\d])(?<default>[^{}]+))?}/
+        // );
+        const re = new RegExp(/\${(?<replacer>[^{}?:]+?)(?<optional>[?]?):((?<scope>[glts]):)?(?<property>[^{}]+)}/);
         const match = re.exec(value);
         if (match) {
             const replacer = match.groups.replacer;
             const optional = match.groups.optional;
             const scope = match.groups.scope;
             const property = match.groups.property;
-            const selector = match.groups.selector;
-            const defaultOperator = match.groups.defaultOperator;
-            const defaultValue = match.groups.default;
+            // const selector = match.groups.selector;
+            // const defaultOperator = match.groups.defaultOperator;
+            // const defaultValue = match.groups.default;
 
-            return new PropertyParser(replacer, !!optional, scope, property, selector, { operator: defaultOperator, value: defaultValue });
+            return new PropertyParser(replacer, !!optional, scope, property);
+            // return new PropertyParser(replacer, !!optional, scope, property, selector, { operator: defaultOperator, value: defaultValue });
         } else {
             return null;
         }
