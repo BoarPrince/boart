29dca8e0f7f4dc15691f3a2520235ff7c96c196c
Markus Kern <mk@jitpay.eu>
2024-03-20 16:45:41 +0100

    add proxies to index.ts

--- src/packages/ui-selenium/src/index.ts
+++ src/packages/ui-selenium/src/index.ts
@@ -10,6 +10,10 @@ import { SelectProxy } from './ui-proxy/SelectProxy';
 import { RadioProxy } from './ui-proxy/RadioProxy';
 import { RadioGroupProxy } from './ui-proxy/RadioGroupProxy';
 import { TableProxy } from './ui-proxy/TableProxy';
+import { InputButtonProxy } from './ui-proxy/InputButtonProxy';
+import { InputProxy } from './ui-proxy/InputProxy';
+import { InputSubmitButtonProxy } from './ui-proxy/InputSubmitButtonProxy';
+import { BySubmit } from './element-locator/BySubmit';
 
 /**
  *
@@ -29,12 +33,16 @@ export default function initialize(): void {
 
     ElementAdapterLocatorHandler.instance.addLocator(new ById());
     ElementAdapterLocatorHandler.instance.addLocator(new ByText());
+    ElementAdapterLocatorHandler.instance.addLocator(new BySubmit());
 
     UIElementProxyHandler.instance.addProxy('table', new TableProxy());
     UIElementProxyHandler.instance.addProxy('button', new ButtonProxy());
     UIElementProxyHandler.instance.addProxy('div', new DivProxy());
     UIElementProxyHandler.instance.addProxy('select', new SelectProxy());
     UIElementProxyHandler.instance.addProxy('input', new RadioProxy());
+    UIElementProxyHandler.instance.addProxy('input', new InputButtonProxy());
+    UIElementProxyHandler.instance.addProxy('input', new InputSubmitButtonProxy());
+    UIElementProxyHandler.instance.addProxy('input', new InputProxy());
     UIElementProxyHandler.instance.addProxy('*', new RadioGroupProxy());
 }
 
